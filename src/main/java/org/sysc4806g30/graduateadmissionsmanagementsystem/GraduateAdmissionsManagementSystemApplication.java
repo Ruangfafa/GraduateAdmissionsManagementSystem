1ae8f9e654daae0b87fe9c121cb3a0efd70c9362
Ruangfafa <clksdysjsh0317@gmail.com>
2024-10-12 20:44:23 -0400

    Initial commit

---
+++ src/main/java/org/sysc4806g30/graduateadmissionsmanagementsystem/GraduateAdmissionsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.sysc4806g30.graduateadmissionsmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateAdmissionsManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduateAdmissionsManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sysc4806g30/graduateadmissionsmanagementsystem/GraduateAdmissionsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sysc4806g30.graduateadmissionsmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateAdmissionsManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
