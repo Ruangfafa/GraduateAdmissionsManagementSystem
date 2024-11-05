aa9ce1b541b087abcea1119dc43a59bca73ae718
rebecca <rebeccali4@cmail.carleton.ca>
2024-11-04 23:29:06 -0500

    get rid of unused import

--- src/main/java/org/sysc4806g30/graduateadmissionsmanagementsystem/loginplatform/LoginPlatformController.java
+++ src/main/java/org/sysc4806g30/graduateadmissionsmanagementsystem/loginplatform/LoginPlatformController.java
@@ -1,6 +1,5 @@
 package org.sysc4806g30.graduateadmissionsmanagementsystem.loginplatform;
 
-import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
